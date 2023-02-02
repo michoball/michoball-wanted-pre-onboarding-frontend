478ba27dbabafc24bdb8dadfe18fafddbce10c45
michoball <myunghun0114@gmail.com>
2023-02-02 19:51:34 +0900

    feat : Add simple axios instance setting file

---
+++ src/utils/api.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+export const api = axios.create({
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  (error) => {
+    if (error instanceof AxiosError) {
+      let errorMessage = "something wrong from server";
+      if (error && error.message) {
+        errorMessage = error.message;
+      }
+      throw new Error(errorMessage);
+    }
+  }
+);
