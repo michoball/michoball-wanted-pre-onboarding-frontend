94b88b66068dea9ae12f517dbb2a9a59b0f37f25
michoball <myunghun0114@gmail.com>
2023-02-03 19:26:34 +0900

    feat : Connect to sign in and sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "@pages/Home";
+import SignIn from "@pages/SignIn";
+import SignUp from "@pages/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
