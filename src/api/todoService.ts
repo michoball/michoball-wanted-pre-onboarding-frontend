860f3738172c9af63fbc589e2e25300cc4992452
michoball <myunghun0114@gmail.com>
2023-02-04 19:18:06 +0900

    feat : Add todo service

---
+++ src/api/todoService.ts
@@ -0,0 +1,45 @@
+import { api } from "@utils/api";
+
+export interface Todo {
+  todo: string;
+}
+
+export interface ITodoData extends Todo {
+  id: number;
+  isCompleted: boolean;
+}
+
+const getAllTodo = async () => {
+  const response = await api.get<ITodoData[]>("/todos");
+  return response.data;
+};
+
+const createTodo = async (todo: string) => {
+  const response = await api.post<ITodoData>("/todos", {
+    todo: todo,
+  });
+  return response.data;
+};
+
+const updateTodo = async (todoData: ITodoData) => {
+  const { todo, id, isCompleted } = todoData;
+  const response = await api.put<{ data: ITodoData }>(`/todos/${id}`, {
+    todo,
+    isCompleted,
+  });
+  return response.data;
+};
+
+const deleteTodo = async (id: number) => {
+  const response = await api.delete(`/todos/${id}`);
+  return response.data;
+};
+
+const TodoService = {
+  getAllTodo,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+};
+
+export default TodoService;
