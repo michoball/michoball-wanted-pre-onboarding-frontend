6e8820c21db6f4cdb530fc4d9ec0f05831a35983
michoball <myunghun0114@gmail.com>
2023-02-04 19:31:51 +0900

    refactor : add setUserToken context

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { StorageControl } from "@utils/localStorage";
+import TodoContext from "context/todoContext";
 
 const PrivateRoute = () => {
   const [loggedIn, setLoggedIn] = useState(true);
+  const { setUserToken } = useContext(TodoContext);
 
   useEffect(() => {
     const userToken = StorageControl.storageGetter("token");
     const userEmail = StorageControl.storageGetter("email");
     if (!userToken || !userEmail) {
       setLoggedIn(false);
+    } else {
+      setUserToken(userToken);
     }
-  }, []);
+  }, [setUserToken]);
 
   return loggedIn ? <Outlet /> : <Navigate to="/signin" />;
 };
