15eca532d5eae3b81f92705ebddd207c86356bc3
michoball <myunghun0114@gmail.com>
2023-02-04 18:00:06 +0900

    fix : Change todo route path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "@pages/Home";
 import SignIn from "@pages/SignIn";
 import SignUp from "@pages/SignUp";
-import { Todo } from "@pages/Todo";
+import Todo from "@pages/Todo";
 import PrivateRoute from "@components/PrivateRoute";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/todo" element={<PrivateRoute />}>
-          <Route index element={<Todo />} />
+          <Route path="/todo" element={<Todo />} />
         </Route>
       </Route>
     </Routes>
