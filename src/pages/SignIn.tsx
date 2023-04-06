7df7c05b8f2ca01a850c89b5ca13cd79761e2597
michoball <myunghun0114@gmail.com>
2023-04-06 23:45:23 +0900

    refactor: Delete Router check code

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,17 +1,7 @@
 import SignInForm from "@components/SignInForm";
-import { useAuth } from "context/authContext";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const SignIn = () => {
-  const navigate = useNavigate();
-  const { isLoggedIn } = useAuth();
-
-  useEffect(() => {
-    if (isLoggedIn) navigate("/todo");
-  }, [isLoggedIn, navigate]);
-
   return (
     <SignInSection>
       <header>
