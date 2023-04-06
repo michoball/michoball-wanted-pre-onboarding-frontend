d28038055b732a8144657f4d342c487b3c65495f
michoball <myunghun0114@gmail.com>
2023-04-06 23:45:04 +0900

    feat: Apply useRouter hook

--- src/layout/AuthGuardLayout.tsx
+++ src/layout/AuthGuardLayout.tsx
@@ -2,8 +2,9 @@ import Navigation from "@pages/Navigation";
 import Spinner from "@styles/spinner/Spinner";
 import { StorageControl } from "@utils/localStorage";
 import { useTodo } from "context/todoContext";
+import { useRouter } from "hooks/useRouter";
 import React, { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 interface AuthGuardLayoutProps {
   children: React.ReactNode;
@@ -11,33 +12,32 @@ interface AuthGuardLayoutProps {
 
 const AuthGuardLayout: React.FC<AuthGuardLayoutProps> = ({ children }) => {
   const [userProfile, setUserProfile] = useState<string | null>(null);
-  const navigate = useNavigate();
+  const { routeTo } = useRouter();
   const { setUserToken } = useTodo();
 
   const fetchUserProfile = useCallback(() => {
     const userProfileResponse = StorageControl.storageGetter("token");
 
     if (userProfileResponse === null) {
-      navigate("/signin");
+      routeTo("/signin");
       return;
     }
 
     setUserProfile(userProfileResponse);
     setUserToken(userProfileResponse);
-  }, [navigate, setUserToken]);
+  }, [routeTo, setUserToken]);
 
   useEffect(() => {
     fetchUserProfile();
-  }, [fetchUserProfile]);
-
-  if (!userProfile) {
-    navigate("/signin");
-    return <></>;
-  }
+  }, [fetchUserProfile, routeTo]);
 
   if (userProfile === null) return <Spinner />;
 
-  return <Navigation>{children}</Navigation>;
+  return userProfile ? (
+    <Navigation>{children}</Navigation>
+  ) : (
+    <Navigate to="/signin" />
+  );
 };
 
 export default AuthGuardLayout;
