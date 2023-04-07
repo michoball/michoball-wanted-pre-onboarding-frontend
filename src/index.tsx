2acd5a449438eb1c5b3a042c5c287e5ad3487cf8
michoball <myunghun0114@gmail.com>
2023-04-07 18:41:14 +0900

    document: Install react-query

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthContextProvider } from "context/authContext";
-import { TodoProvider } from "context/todoContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <TodoProvider>
-        <App />
-      </TodoProvider>
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
