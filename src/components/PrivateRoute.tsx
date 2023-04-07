913ecc6197ff29b7eca6c19dbfbed4d1758253cb
michoball <myunghun0114@gmail.com>
2023-04-07 18:44:06 +0900

    refactor: Delete useAuth context hook

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "context/authContext";
 import Navigation from "@pages/Navigation";
 import { useRouter } from "hooks/useRouter";
+import { StorageControl } from "@utils/localStorage";
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const { isLoggedIn } = useAuth();
+  const userToken = StorageControl.storageGetter("token");
   const { currentPath } = useRouter();
 
   if (currentPath === "/") {
     return <Navigation>{children}</Navigation>;
   }
 
-  return !isLoggedIn ? (
+  return !userToken ? (
     <Navigation>{children}</Navigation>
   ) : (
     <Navigate to="/todo" />
