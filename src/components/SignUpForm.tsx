509b4890ca857dc9cffe9f4c95fd549e815834c6
michoball <myunghun0114@gmail.com>
2023-04-11 19:51:01 +0900

    feat: Apply react-hook-form useform hook in signUp and decalre SignUpInput type

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,121 +1,111 @@
-import { ChangeEvent, FormEvent, useEffect, useReducer, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import AppButton from "@styles/button/AppButton";
 import FormInput from "@styles/FormInput";
 import { useAuth } from "context/authContext";
-import {
-  ActionType,
-  AuthForm,
-  emailReducer,
-  passwordReducer,
-  State,
-} from "./SignInForm";
+import { AuthForm, LoginInputs } from "./SignInForm";
 import { useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+import {
+  emailExpression,
+  emailRegex,
+  passwordExpression,
+  passwordRegex,
+} from "@utils/regex";
 
-const confirmPasswordReducer = (state: State, action: ActionType): State => {
-  if (action.type === "USER_INPUT") {
-    if (action.val)
-      return { value: action.val, isValid: action.val.trim().length >= 8 };
-  }
-  return { value: "", isValid: null };
-};
+type SignUpInputs = { passwordConfirm: string } & LoginInputs;
 
 const SignUpForm = () => {
   const { onSignUp } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    formState: { isValid },
+    watch,
+  } = useForm<SignUpInputs>();
   const navigate = useNavigate();
-  const [formIsValid, setFormIsValid] = useState(false);
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: "",
-    isValid: null,
-  });
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
-    value: "",
-    isValid: null,
-  });
-  const [confirmPasswordState, dispatchConfirmPassword] = useReducer(
-    confirmPasswordReducer,
-    {
-      value: "",
-      isValid: null,
-    }
-  );
+  const [emailIsValid, setEmailIsvalid] = useState<boolean | null>(null);
+  const [passwordIsValid, setPasswordIsValid] = useState<boolean | null>(null);
+  const [passwordConfirmIsValid, setPasswordConfirmIsValid] = useState<
+    boolean | null
+  >(null);
 
-  const { value: email, isValid: emailIsValid } = emailState;
-  const { value: password, isValid: passwordIsValid } = passwordState;
-  const { value: confirmPassword, isValid: confirmPasswordIsValid } =
-    confirmPasswordState;
-
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      setFormIsValid(
-        !!emailIsValid && !!passwordIsValid && !!confirmPasswordIsValid
-      );
-    }, 300);
-
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [emailIsValid, passwordIsValid, confirmPasswordIsValid]);
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (formIsValid) {
+  const onSubmit: SubmitHandler<SignUpInputs> = (data) => {
+    const { email, password } = data;
+    if (isValid) {
       onSignUp(email, password);
       navigate("/signin");
     }
-    dispatchEmail({ type: "RESET" });
-    dispatchPassword({ type: "RESET" });
-    dispatchConfirmPassword({ type: "RESET" });
+    resetField("email");
+    resetField("password");
   };
 
   const emailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+    const emailInput = watch("email");
+    setEmailIsvalid(emailRegex(emailInput));
   };
 
   const passwordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+    const passwordInput = watch("password");
+    setPasswordIsValid(passwordRegex(passwordInput));
   };
 
   const confirmPasswordChangeHandler = (
     event: ChangeEvent<HTMLInputElement>
   ) => {
-    dispatchConfirmPassword({ type: "USER_INPUT", val: event.target.value });
+    const passwordInput = watch("password");
+    const confirmPassword = watch("passwordConfirm");
+    setPasswordConfirmIsValid(confirmPassword === passwordInput);
   };
 
   return (
     <>
-      <AuthForm onSubmit={handleSubmit}>
+      <AuthForm onSubmit={handleSubmit(onSubmit)}>
         <FormInput
           label="Email"
           type="email"
           data-testid="email-input"
-          value={email}
-          required
           isValid={emailIsValid}
-          onChange={emailChangeHandler}
+          {...register("email", {
+            required: true,
+            onChange: emailChangeHandler,
+            pattern: {
+              value: emailExpression,
+              message: "이메일 형식에 맞지 않습니다.",
+            },
+          })}
         />
         <FormInput
           label="Password"
           type="password"
           data-testid="password-input"
-          value={password}
-          required
           isValid={passwordIsValid}
-          onChange={passwordChangeHandler}
+          {...register("password", {
+            required: true,
+            onChange: passwordChangeHandler,
+            pattern: {
+              value: passwordExpression,
+              message: "8자리 이상 비밀번호를 사용하세요.",
+            },
+          })}
         />
         <FormInput
           label="Confirm Password"
           type="password"
           required
-          value={confirmPassword}
-          isValid={confirmPasswordIsValid}
-          onChange={confirmPasswordChangeHandler}
+          isValid={passwordConfirmIsValid}
+          {...register("passwordConfirm", {
+            required: true,
+            onChange: confirmPasswordChangeHandler,
+            validate: (value) => watch("password") === value,
+          })}
         />
         <AppButton
           type="submit"
           data-testid="signup-button"
-          isNotValid={!formIsValid}
+          isNotValid={!isValid}
         >
-          {formIsValid ? "Sign Up" : <span>&#10005;</span>}
+          {isValid ? "Sign Up" : <span>&#10005;</span>}
         </AppButton>
       </AuthForm>
     </>
